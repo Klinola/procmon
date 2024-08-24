7ccfef9ea129f8212ea3e4624e1a6d1d682710e3
Klinola <KelinerALOC@outlook.com>
2024-08-24 16:42:22 +0800

    Add util funcs back

--- procmon.c
+++ procmon.c
@@ -222,6 +222,279 @@ void *thread_nft(void *arg) {
     }
 }
 
+void get_cpuoccupy(CPU_OCCUPY *cpu) {
+    char buff[BUFFSIZE];
+    FILE *fd;
+
+    fd = fopen("/proc/stat", "r");
+    if (fd == NULL) {
+        perror("open /proc/stat failed\n");
+        exit(0);
+    }
+    fgets(buff, sizeof(buff), fd);
+
+    sscanf(buff, "%s %u %u %u %u", cpu->name, &cpu->user, &cpu->nice, &cpu->system, &cpu->idle);
+    fclose(fd);
+}
+
+double cal_occupy(CPU_OCCUPY *c1, CPU_OCCUPY *c2) {
+    double t1, t2;
+    double id, sd;
+    double cpu_used;
+
+    t1 = (double)(c1->user + c1->nice + c1->system + c1->idle);
+    t2 = (double)(c2->user + c2->nice + c2->system + c2->idle);
+
+    id = (double)(c2->user - c1->user);
+    sd = (double)(c2->system - c1->system);
+
+    cpu_used = (100 * (id + sd) / (t2 - t1));
+    return cpu_used;
+}
+
+void open_sysinfo() {
+    int ret = 0;
+    ret = sysinfo(&info);
+    if (ret != 0) {
+        perror("get sys_info failed\n");
+        exit(0);
+    }
+}
+
+void get_mem_usage(double *mem_used) {
+    FILE *fd;
+
+    fd = fopen("/proc/meminfo", "r");
+    if (fd == NULL) {
+        perror("open /proc/meminfo failed\n");
+        exit(0);
+    }
+
+    size_t bytes_read;
+    size_t read;
+    char *line = NULL;
+    int index = 0;
+    int avimem = 0;
+
+    while ((read = getline(&line, &bytes_read, fd)) != -1) {
+        if (++index <= 2) {
+            continue;
+        }
+        if (strstr(line, "MemAvailable") != NULL) {
+            sscanf(line, "%*s%d%*s", &avimem);
+            break;
+        }
+    }
+    free(line);
+    int t = info.totalram / 1024.0;
+    *mem_used = (t - avimem) * 100 / t;
+
+#if DEBUG
+    printf("\n");
+    printf("total memory: %.3f\n", info.totalram * 1.0 / 1024 / 1024 / 1024);
+    printf("free memory: %.3f\n", info.freeram * 1.0 / 1024 / 1024 / 1024);
+    printf("shared memory: %.3f\n", info.sharedram * 1.0 / 1024 / 1024 / 1024);
+    printf("buffer memory: %.3f\n", info.bufferram * 1.0 / 1024 / 1024 / 1024);
+    printf("totalswap memory: %.3f\n", info.totalswap * 1.0 / 1024 / 1024 / 1024);
+    printf("freeswap memory: %.3f\n", info.freeswap * 1.0 / 1024 / 1024 / 1024);
+#endif
+
+    fclose(fd);
+}
+
+void get_cpu_usage(double *cpu_used) {
+    CPU_OCCUPY cpu_0, cpu_1;
+
+    get_cpuoccupy(&cpu_0);
+    sleep(1);
+    get_cpuoccupy(&cpu_1);
+
+    *cpu_used = cal_occupy(&cpu_0, &cpu_1);
+
+#if DEBUG
+    printf("cpu: %.2f%%\n", *cpu_used);
+#endif
+}
+
+void get_host_runtime(int *hours, int *minutes) {
+    *hours = info.uptime / 3600;
+    *minutes = (info.uptime % 3600) / 60;
+}
+
+void open_netconf(FILE **fd) {
+    *fd = fopen("/proc/net/dev", "r");
+    if (*fd == NULL) {
+        perror("open file /proc/net/dev failed!\n");
+        exit(0);
+    }
+}
+
+void show_netinterfaces(NET_INTERFACE *p_net, int n) {
+    NET_INTERFACE *temp;
+    temp = p_net;
+
+    while (temp != NULL) {
+        if (!n) {
+            printf("Interface: %-16s\t IP:%16s\t MAC:%12s\n", temp->name, temp->ip, temp->mac);
+        } else {
+            printf("Interface: %-16s\t", temp->name);
+            if (temp->speed_level & 0x1) {
+                printf("download:%10.2lfMB/s\t\t", temp->d_speed);
+            } else {
+                printf("download:%10.2lfKB/s\t\t", temp->d_speed);
+            }
+
+            if ((temp->speed_level >> 1) & 0x1) {
+                printf("upload:%10.2lfMB/s\n", temp->u_speed);
+            } else {
+                printf("upload:%10.2lfKB/s\n", temp->u_speed);
+            }
+        }
+
+        temp = temp->next;
+    }
+}
+
+int get_interface_info(NET_INTERFACE **net, int *n) {
+    int fd;
+    int num = 0;
+    struct ifreq buf[16];
+    struct ifconf ifc;
+
+    NET_INTERFACE *p_temp = NULL;
+    (*net)->next = NULL;
+
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        close(fd);
+        printf("socket open failed\n");
+    }
+
+    ifc.ifc_len = sizeof(buf);
+    ifc.ifc_buf = (caddr_t)buf;
+
+    if (!ioctl(fd, SIOCGIFCONF, (char *)&ifc)) {
+        num = ifc.ifc_len / sizeof(struct ifreq);
+        *n = num;
+
+        while (num-- > 0) {
+            strcpy((*net)->name, buf[num].ifr_name);
+
+            if (!(ioctl(fd, SIOCGIFADDR, (char *)&buf[num]))) {
+                memset((*net)->ip, 0, 16);
+                strcpy((*net)->ip, inet_ntoa(((struct sockaddr_in *)(&buf[num].ifr_addr))->sin_addr));
+            }
+
+            if (!ioctl(fd, SIOCGIFHWADDR, (char *)(&buf[num]))) {
+                memset((*net)->mac, 0, 13);
+                snprintf((*net)->mac, 13, "%02x%02x%02x%02x%02x%02x",
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[0],
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[1],
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[2],
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[3],
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[4],
+                         (unsigned char)buf[num].ifr_hwaddr.sa_data[5]);
+            }
+            if (num >= 1) {
+                p_temp = (NET_INTERFACE *)malloc(sizeof(NET_INTERFACE));
+                memset(p_temp, 0, sizeof(NET_INTERFACE));
+                p_temp->next = *net;
+                *net = p_temp;
+            }
+        }
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+void get_rtx_bytes(char *name, RTX_BYTES *rtx) {
+    char *line = NULL;
+    size_t bytes_read;
+    size_t read;
+    char str1[32];
+    char str2[32];
+    int i = 0;
+    open_netconf(&net_dev_file);
+    gettimeofday(&rtx->rtx_time, NULL);
+
+    while ((read = getline(&line, &bytes_read, net_dev_file)) != -1) {
+        if ((++i) <= 2)
+            continue;
+        if (strstr(line, name) != NULL) {
+            memset(str1, 0x0, 32);
+            memset(str2, 0x0, 32);
+            sscanf(line, "%*s%s%*s%*s%*s%*s%*s%*s%*s%s", str1, str2);
+            rtx->tx_bytes = atol(str2);
+            rtx->rx_bytes = atol(str1);
+        }
+    }
+
+    free(line);
+    fclose(net_dev_file);
+}
+
+void rtx_bytes_copy(RTX_BYTES *dest, RTX_BYTES *src) {
+    dest->rx_bytes = src->rx_bytes;
+    dest->tx_bytes = src->tx_bytes;
+    dest->rtx_time = src->rtx_time;
+    src->rx_bytes = 0;
+    src->tx_bytes = 0;
+}
+
+void get_network_speed(NET_INTERFACE *p_net) {
+    RTX_BYTES rtx0, rtx1;
+    NET_INTERFACE *temp1, *temp2, *temp3;
+    temp1 = p_net;
+    temp2 = p_net;
+    temp3 = p_net;
+    while (temp1 != NULL) {
+        get_rtx_bytes(temp1->name, &rtx0);
+        temp1->rtx0_cnt.tx_bytes = rtx0.tx_bytes;
+        temp1->rtx0_cnt.rx_bytes = rtx0.rx_bytes;
+        temp1->rtx0_cnt.rtx_time = rtx0.rtx_time;
+        temp1 = temp1->next;
+    }
+
+    sleep(WAIT_SECOND);
+
+    while (temp2 != NULL) {
+        get_rtx_bytes(temp2->name, &rtx1);
+        temp2->rtx1_cnt.tx_bytes = rtx1.tx_bytes;
+        temp2->rtx1_cnt.rx_bytes = rtx1.rx_bytes;
+        temp2->rtx1_cnt.rtx_time = rtx1.rtx_time;
+        temp2->speed_level &= 0x00;
+        temp2 = temp2->next;
+    }
+    while (temp3 != NULL) {
+        cal_netinterface_speed(&temp3->u_speed, &temp3->d_speed, &temp3->speed_level,
+                               (&temp3->rtx0_cnt), (&temp3->rtx1_cnt));
+        temp3 = temp3->next;
+    }
+}
+
+void cal_netinterface_speed(double *u_speed, double *d_speed, unsigned char *level,
+                            RTX_BYTES *rtx0, RTX_BYTES *rtx1) {
+    long int time_lapse;
+    time_lapse = (rtx1->rtx_time.tv_sec * 1000 + rtx1->rtx_time.tv_usec / 1000) -
+                 (rtx0->rtx_time.tv_sec * 1000 + rtx0->rtx_time.tv_usec / 1000);
+    *d_speed = (rtx1->rx_bytes - rtx0->rx_bytes) * 1.0 / (1024 * time_lapse * 1.0 / 1000);
+    *u_speed = (rtx1->tx_bytes - rtx0->tx_bytes) * 1.0 / (1024 * time_lapse * 1.0 / 1000);
+
+    if (*d_speed >= MB * 1.0) {
+        *d_speed = *d_speed / 1024;
+        *level |= 0x1;
+    } else {
+        *level &= 0xFE;
+    }
+
+    if (*u_speed >= MB * 1.0) {
+        *u_speed = *u_speed / 1024;
+        *level |= 0x1 << 1;
+    } else {
+        *level &= 0xFD;
+    }
+}
+
 // 实现 gRPC 服务类
 class ProcmonServiceImpl final : public procmon::ProcmonService::Service {
 public:
